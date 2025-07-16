b2626b1996fa389a95827d8f57d41f668899c163
Abdul Rafey <abdulrafey12389@gmail.com>
2025-07-16 10:34:59 -0700

    backend start

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 //NODE MODULES...
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
+// INITIALIZE APP...
 const app = express();
+dotenv.config();
+
+// CUSTOM VARIABLES...
 const PORT = process.env.PORT || 5000;
+
+// MIDDLEWARES...
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('working');
