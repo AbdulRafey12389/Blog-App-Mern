e6f9f9a6af9769121871dc1327beb96346dc2062
AbdulRafey <abdulrafey12389@gmail.com>
2025-07-18 17:07:35 +0500

    temprolarly chechk

--- backend/src/controller/v1/blog/getPublicBlogs.js
+++ backend/src/controller/v1/blog/getPublicBlogs.js
@@ -1,6 +1,8 @@
+import { connectToDatabase } from '../../../config/db.js';
 import Blog from '../../../models/blog.model.js';
 
 const getPublicBlogs = async (req, res) => {
+  await connectToDatabase();
   try {
     const blogs = await Blog.find({ isPublic: true }).populate({
       path: 'author',
